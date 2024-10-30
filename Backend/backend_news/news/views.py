3af88ea69f24a0234eb69cf8aaabad61f3e7ffef
Adrianm7777 <Adrianmaterka10@gmail.com>
2024-10-30 17:40:24 +0100

    feature: created a view to return a list of articles

--- Backend/backend_news/news/views.py
+++ Backend/backend_news/news/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from .models import Article
+from .serializers import ArticleSerialzer
 
-# Create your views here.
+@api_view(['GET'])
+def article_list(request):
+    articles = Article.objects.all()
+    serialzers = ArticleSerialzer(articles, many=True)
+    return Response(serialzers.data)
