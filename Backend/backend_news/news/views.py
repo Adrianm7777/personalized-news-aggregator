9f983ecc8f09e3730db088d585ce83ee1eb13372
Adrianm7777 <Adrianmaterka10@gmail.com>
2024-10-31 17:34:19 +0100

    feature: Added article summarization using transformers in Django

--- Backend/backend_news/news/views.py
+++ Backend/backend_news/news/views.py
@@ -3,9 +3,17 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from .models import Article
 from .serializers import ArticleSerialzer
+from transformers import pipeline
 
 @api_view(['GET'])
 def article_list(request):
     articles = Article.objects.all()
     serialzers = ArticleSerialzer(articles, many=True)
+    for article in articles:
+        article.content = summerize_text(article.content)
     return Response(serialzers.data)
+
+summerizer = pipeline("summarization")
+def summerize_text(text):
+    summary = summerizer(text,max_lenght=50, min_lenght=25, do_sample=False)
+    return summary[0]['summary_text']
